8ecefbc268d823f72f350b8e3aafd47d414c0d29
Stephane Thomas <thomas.stephn@gmail.com>
2013-08-01 20:53:18 +0200

    Add basic ajax call to send requst to server, print in console modified:   public/js/application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/beautiful',
+      data: $(this).serialize()
+    }).done(function(response){
+      console.log(response.beautiful_text);
+    });
+  });
 });
